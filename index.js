d08d2ca9e4dc00eb63c9685cbb444a08f5af4534
Alicia Petgrave <alicia.petgrave@gmail.com>
2019-12-19 14:15:47 -0500

    10 passing

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor (name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(somefood){
+    if (this.stomach.length < 10){
+    return this.stomach.push(somefood);
+    }
+  }
+  poop(){
+    return this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
